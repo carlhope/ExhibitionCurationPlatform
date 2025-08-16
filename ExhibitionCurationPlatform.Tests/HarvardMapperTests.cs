8f71ba7cda17623213dc94e703d263049da3ba82
Carl hope <84421131+carlhope@users.noreply.github.com>
2025-08-16 16:56:35 +0100

    Create mapping tests

---
+++ ExhibitionCurationPlatform.Tests/HarvardMapperTests.cs
@@ -0,0 +1,54 @@
+﻿using ExhibitionCurationPlatform.Mappers;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Text.Json;
+using System.Threading.Tasks;
+
+namespace ExhibitionCurationPlatform.Tests
+{
+    public class HarvardMapperTests
+    {
+        [Fact]
+        public void FromHarvardJson_MapsAllFieldsCorrectly()
+        {
+            var json = JsonDocument.Parse("""
+    {
+        "id": "abc123",
+        "title": "Sunflowers",
+        "people": [{ "name": "Van Gogh" }],
+        "primaryimageurl": "http://example.com/image.jpg",
+        "dated": "1888"
+    }
+    """).RootElement;
+
+            var result = ArtworkMapper.FromHarvardJson(json);
+
+            Assert.Equal("abc123", result.Id);
+            Assert.Equal("Sunflowers", result.Title);
+            Assert.Equal("Van Gogh", result.Artist);
+            Assert.Equal("http://example.com/image.jpg", result.ImageUrl);
+            Assert.Equal("1888", result.Date);
+            Assert.Equal("Harvard", result.Source);
+        }
+
+        [Fact]
+        public void FromHarvardJson_HandlesEmptyPeopleArray()
+        {
+            var json = JsonDocument.Parse("""
+    {
+        "id": "abc123",
+        "title": "Sunflowers",
+        "people": [],
+        "primaryimageurl": "http://example.com/image.jpg",
+        "dated": "1888"
+    }
+    """).RootElement;
+
+            var result = ArtworkMapper.FromHarvardJson(json);
+
+            Assert.Equal("Unknown", result.Artist);
+        }
+    }
+}
---
+++ ExhibitionCurationPlatform.Tests/MetMapperTests.cs
@@ -0,0 +1,51 @@
+﻿using ExhibitionCurationPlatform.Mappers;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Text.Json;
+using System.Threading.Tasks;
+
+namespace ExhibitionCurationPlatform.Tests
+{
+    public class MetMapperTests
+    {
+        [Fact]
+        public void FromMetMuseumJson_MapsAllFieldsCorrectly()
+        {
+            var json = JsonDocument.Parse("""
+    {
+        "objectID": 12345,
+        "title": "Sunflowers",
+        "artistDisplayName": "Vincent van Gogh",
+        "primaryImage": "http://example.com/image.jpg",
+        "objectDate": "1888"
+    }
+    """).RootElement;
+
+            var result = ArtworkMapper.FromMetMuseumJson(json);
+
+            Assert.Equal("12345", result.Id);
+            Assert.Equal("Sunflowers", result.Title);
+            Assert.Equal("Vincent van Gogh", result.Artist);
+            Assert.Equal("http://example.com/image.jpg", result.ImageUrl);
+            Assert.Equal("1888", result.Date);
+            Assert.Equal("Met", result.Source);
+        }
+
+        [Fact]
+        public void FromMetMuseumJson_HandlesMissingFieldsGracefully()
+        {
+            var json = JsonDocument.Parse("{}").RootElement;
+
+            var result = ArtworkMapper.FromMetMuseumJson(json);
+
+            Assert.False(string.IsNullOrWhiteSpace(result.Id)); // Should be a GUID
+            Assert.Equal("Untitled", result.Title);
+            Assert.Equal("Unknown", result.Artist);
+            Assert.Null(result.ImageUrl);
+            Assert.Equal("Date unknown", result.Date);
+            Assert.Equal("Met", result.Source);
+        }
+    }
+}
