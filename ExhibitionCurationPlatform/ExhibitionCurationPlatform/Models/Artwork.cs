bf544b80bb58d02e4be1ddba7e0faa73a3fa8bb1
Carl hope <84421131+carlhope@users.noreply.github.com>
2025-09-03 16:17:24 +0100

    add [Required] to model fields

--- ExhibitionCurationPlatform/ExhibitionCurationPlatform/Models/Artwork.cs
+++ ExhibitionCurationPlatform/ExhibitionCurationPlatform/Models/Artwork.cs
@@ -1,11 +1,17 @@
-﻿namespace ExhibitionCurationPlatform.Models
+﻿using System.ComponentModel.DataAnnotations;
+
+namespace ExhibitionCurationPlatform.Models
 {
     public class Artwork
     {
         public string Id { get; set; } = Guid.NewGuid().ToString();
+        [Required]
         public string Title { get; set; }
+        [Required]
         public string Artist { get; set; }
+        [Required]
         public string ImageUrl { get; set; }
+        [Required]
         public string Date { get; set; }
         public string Source { get; set; } // "Met" or "Harvard"
         public Guid? ExhibitionId { get; set; }//nullable to allow artworks not yet assigned to an exhibition
