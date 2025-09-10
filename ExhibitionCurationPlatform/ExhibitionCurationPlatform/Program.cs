bda6ad884cf425f4cc0620bc4e4ec02db0a442c3
Carl hope <84421131+carlhope@users.noreply.github.com>
2025-09-10 11:04:36 +0100

    moved connection string into user secrets to prevent azure connection string appearing in git history.

--- ExhibitionCurationPlatform/ExhibitionCurationPlatform/Program.cs
+++ ExhibitionCurationPlatform/ExhibitionCurationPlatform/Program.cs
@@ -20,7 +20,6 @@ namespace ExhibitionCurationPlatform
         {
             var builder = WebApplication.CreateBuilder(args);
             var connectionString = builder.Configuration.GetConnectionString("ApplicationDbContextConnection") ?? throw new InvalidOperationException("Connection string 'ApplicationDbContextConnection' not found.");;
-
             builder.Services.AddDbContext<ApplicationDbContext>(options =>
                 //options.UseInMemoryDatabase("ExhibitionDb"));
                 options.UseSqlServer(connectionString));
