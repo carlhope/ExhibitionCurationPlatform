7f2197ef71deecc8e777ff99e023b2de12d9b94b
Carl hope <84421131+carlhope@users.noreply.github.com>
2025-09-10 15:53:08 +0100

    remove duplicaton and specify is required in models

--- ExhibitionCurationPlatform/ExhibitionCurationPlatform/Models/Artwork.cs
+++ ExhibitionCurationPlatform/ExhibitionCurationPlatform/Models/Artwork.cs
@@ -6,18 +6,18 @@ namespace ExhibitionCurationPlatform.Models
     {
         public string Id { get; set; } = Guid.NewGuid().ToString();
         [Required]
-        public string Title { get; set; }
+        public required string Title { get; set; }
         public string? Description { get; set; }
         [Required]
         public string Artist { get; set; }
         [Required]
         [Url]
-        public string ImageUrl { get; set; }
+        public required string ImageUrl { get; set; }
         [Required]
         
         public DateOnly Date { get; set; }
         public string? DateAsString { get; set; }
-        public string Source { get; set; } // "Met" or "Harvard"
+        public string? Source { get; set; } // "Met" or "Harvard"
         public Guid? ExhibitionId { get; set; }//nullable to allow artworks not yet assigned to an exhibition
         public string? CreatedBy { get; set; } // User who added the artwork
         public Exhibition? Exhibition { get; set; }
--- ExhibitionCurationPlatform/ExhibitionCurationPlatform/Models/Exhibition.cs
+++ ExhibitionCurationPlatform/ExhibitionCurationPlatform/Models/Exhibition.cs
@@ -5,9 +5,9 @@ namespace ExhibitionCurationPlatform.Models
     public class Exhibition
     {
         public Guid Id { get; set; } = Guid.NewGuid();
-        public string Title { get; set; }
-        public string Curator { get; set; } // Optional: could be user name or ID
-        public string Description { get; set; }
+        public required string Title { get; set; }
+        public string? Curator { get; set; } // Optional: could be user name or ID
+        public string? Description { get; set; }
         public DateTime CreatedAt { get; set; } = DateTime.UtcNow;
         public DateTime? ScheduledDate { get; set; } // Optional: for future planning
 
